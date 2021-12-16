550b4473e4ff839df188d625dbea430f1057b393
Tomas Bjerre <tomas.bjerre85@gmail.com>
2021-12-16 19:19:07 +0100

    work

---
+++ matchers/example.matcher.js
@@ -0,0 +1,13 @@
+function getName(repoFolder) {
+    return `tomas namn`
+}
+
+function getStartCommand(repoFolder) {
+    return `npm run start`
+}
+
+function isMatching(repoFolder) {
+    return true;
+}
+
+module.exports = { getName, getStartCommand, isMatching }
