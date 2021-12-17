5e5e5bb0ee9db9865cf084d6e97d7185f90bb65f
Tomas Bjerre <tomas.bjerre85@gmail.com>
2021-12-17 16:01:50 +0100

    work

--- matchers/example.matcher.js
+++ matchers/example.matcher.js
@@ -1,17 +1,17 @@
-function getName(repoFolder) {
-    return require(`${repoFolder}/package.json`).name
-}
-
-function getStartCommand(repoFolder) {
-    return `npm run start`
-}
-
 function isMatching(repoFolder) {
     try {
         return require(`${repoFolder}/package.json`).scripts["start"] != undefined
     } catch {
         return false;
     }
+}
+
+function getName(repoFolder) {
+    return require(`${repoFolder}/package.json`).name
+}
+
+function getStartCommand(repoFolder) {
+    return `npm run start`
 }
 
 module.exports = { getName, getStartCommand, isMatching }
